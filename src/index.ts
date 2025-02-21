662d6e835446b7116d36e77fc5a96817f04555a7
22BCS16828 <22BCS16828@cuchd.in>
2025-02-21 17:43:50 +0530

    added cors

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express, { Request, Response } from "express";
 import { postInput } from "./types/types";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 
 app.get("/bfhl", (req: Request, res: Response) => {
   res.status(200).json({
