ae547590698e2d874516ef7df925562a3eb81e54
22BCS16828 <22BCS16828@cuchd.in>
2025-02-21 17:28:10 +0530

    init

---
+++ src/index.ts
@@ -0,0 +1,69 @@
+import express, { Request, Response } from "express";
+import { postInput } from "./types/types";
+
+const app = express();
+app.use(express.json());
+
+app.get("/bfhl", (req: Request, res: Response) => {
+  res.status(200).json({
+    operation_code: 1,
+  });
+});
+
+app.post("/bfhl", async (req: Request, res: Response) => {
+  try {
+    const body = req.body;
+    const parsedResponse = postInput.safeParse(body);
+
+    if (!parsedResponse.success) {
+      res.status(400).json({
+        is_success: false,
+        message: "Invalid data",
+      });
+      return;
+    }
+
+    const inputArray = parsedResponse.data.data;
+    const numbers: number[] = [];
+    const alphabets: string[] = [];
+
+    inputArray.forEach((item) => {
+      if (/^\d+$/.test(item)) {
+        numbers.push(Number(item));
+      } else if (/^[a-zA-Z]$/.test(item)) {
+        // Ensures only single-character alphabets are allowed
+        alphabets.push(item);
+      }
+    });
+
+    console.log(numbers);
+    console.log(alphabets);
+
+    const largestAlphabet =
+      alphabets.length > 0
+        ? alphabets.reduce((max, current) =>
+            current.toLowerCase() > max.toLowerCase() ? current : max
+          )
+        : null;
+
+    res.status(200).json({
+      is_success: true,
+      user_id: "Abhijeet_Singh_22072004",
+      email: "abhijeet16828@gmail.com",
+      roll_number: "22BCS16828",
+      numbers: numbers,
+      alphabets: alphabets,
+      highest_alphabet: largestAlphabet ? [largestAlphabet] : [],
+    });
+    return;
+  } catch (error) {
+    res.status(500).json({
+      is_success: false,
+      message: "Internal server error",
+    });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
